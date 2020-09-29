00e2f1159ae40a9d03e6becea5dce492b6c12211
T.I <T.I@TI.rsatacadao.local>
2020-09-29 12:08:30 -0300

    alteração no arquivo classe GenericDAO adicionado metodos alterar, lista, excluir, verifica criação de banco

--- SistemaMercadoMasterJava/src/dao/GenericDAO.java
+++ SistemaMercadoMasterJava/src/dao/GenericDAO.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
@@ -32,7 +35,104 @@ public class GenericDAO {
 			throw new HibernateException(ex);
 		}
 	}
-	
-	
+    public void alterar(Object obj) throws HibernateException {
+        Session sessao = null;
+        try   {
+          sessao = ConexaoHibernate.getSessionFactory().openSession();
+          sessao.getTransaction().begin();
 
+          sessao.update(obj);
+
+          sessao.getTransaction().commit();
+          sessao.close();
+        } catch ( HibernateException ex) {
+            if ( sessao != null) {
+                sessao.getTransaction().rollback();
+                sessao.close();
+            }
+            throw new HibernateException(ex);
+        }
+    }
+    public List listar( Class classe ) throws HibernateException {
+        Session sessao = null;
+        List lista = null;
+
+        try   {
+          sessao = ConexaoHibernate.getSessionFactory().openSession();
+          sessao.getTransaction().begin();
+
+          Criteria consulta = sessao.createCriteria( classe );
+          lista = consulta.list();
+
+          sessao.getTransaction().commit();
+          sessao.close();
+        } catch ( HibernateException ex) {
+            if ( sessao != null) {
+                sessao.getTransaction().rollback();
+                sessao.close();
+            }
+            throw new HibernateException(ex);
+        }
+
+        return lista;
+    }
+    public void excluir (Object obj) throws HibernateException {
+        Session sessao = null;
+        try   {
+          sessao = ConexaoHibernate.getSessionFactory().openSession();
+          sessao.getTransaction().begin();
+
+          sessao.delete(obj);
+
+          sessao.getTransaction().commit();
+          sessao.close();
+        } catch ( HibernateException ex) {
+            if ( sessao != null) {
+                sessao.getTransaction().rollback();
+                sessao.close();
+            }
+            throw new HibernateException(ex);
+        }          
+    }
+    public Object get(Class classe, int id) throws HibernateException {
+        Session sessao = null;
+        Object objReturn = null;
+        try   {
+          sessao = ConexaoHibernate.getSessionFactory().openSession();
+          sessao.getTransaction().begin();
+
+          objReturn = sessao.get(classe, id );
+
+          sessao.getTransaction().commit();
+          sessao.close();
+        } catch ( HibernateException ex) {
+            if ( sessao != null) {
+                sessao.getTransaction().rollback();
+                sessao.close();
+            }
+            throw new HibernateException(ex);
+        }
+        return objReturn;
+    }
+    public Object load(Class classe, int id) throws HibernateException {
+        Session sessao = null;
+        Object objReturn = null;
+        try   {
+          sessao = ConexaoHibernate.getSessionFactory().openSession();
+          sessao.getTransaction().begin();
+
+          objReturn = sessao.load(classe, id );
+          objReturn.toString();
+
+          sessao.getTransaction().commit();
+          sessao.close();
+        } catch ( HibernateException ex) {
+            if ( sessao != null) {
+                sessao.getTransaction().rollback();
+                sessao.close();
+            }
+            throw new HibernateException(ex);
+        }
+        return objReturn;
+    }
 }
