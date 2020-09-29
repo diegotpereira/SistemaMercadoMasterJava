a64c83addfbb486eeddc9ae4726dd23f2cf2286f
Diego <diegoteixeirapereira@hotmail.com>
2020-09-29 14:16:39 -0300

    alteração no arquivo Menu.java adicionado metodos

--- SistemaMercadoMasterJava/src/controller/InterfaceControl.java
+++ SistemaMercadoMasterJava/src/controller/InterfaceControl.java
@@ -3,5 +3,30 @@ package controller;
 
 
 public class InterfaceControl {
+
+	public void janListarProduto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void janRealizarVenda() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void janListarCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void janCadCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void janCadProduto() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
